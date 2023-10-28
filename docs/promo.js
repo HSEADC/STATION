2eb6b42eb539f0c0f21482c8a57e8b198d7e8a3b
amir <amir.boisov@gmail.com>
2023-10-29 00:48:01 +0300

    added changes to docs

--- docs/promo.js
+++ docs/promo.js
@@ -1,13 +1,6 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 var __webpack_exports__ = {};
- // import React from 'react'
-// import { createRoot } from 'react-dom/client'
-// import ComponentExample from './javascript/ComponentExample.jsx'
-// document.addEventListener('DOMContentLoaded', () => {
-//   const container = document.getElementById('reactComponentRoot')
-//   const root = createRoot(container)
-//   root.render(<ComponentExample />)
-// })
+
 /******/ })()
 ;
