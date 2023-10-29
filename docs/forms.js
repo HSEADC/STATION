d22f484a400c141445c7025bd1012828e4fda29b
amir <amir.boisov@gmail.com>
2023-10-29 19:26:44 +0300

    something

--- docs/forms.js
+++ docs/forms.js
@@ -12,14 +12,14 @@ var svgContainer = document.getElementsByClassName('svgContainer');
 Array.from(svgContainer).forEach(function (container) {
   container.addEventListener('mouseover', function () {
     container.childNodes[1].style.color = 'var(--bg-dark)';
-    container.childNodes[3].style.opacity = 1;
+    container.childNodes[1].style.opacity = 1;
     console.log('In');
   });
 });
 Array.from(svgContainer).forEach(function (container) {
   container.addEventListener('mouseout', function () {
     container.childNodes[1].style.color = 'var(--bg-light)';
-    container.childNodes[3].style.opacity = 0;
+    container.childNodes[1].style.opacity = 0;
   });
 });
 /******/ })()
